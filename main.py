697ddbb6b24cccf5073b7e33a402ea5b3a22581d
niksul <nikita.sulimenko@smddev.com>
2024-04-02 16:38:24 +0300

    del  unused file

--- main.py
+++
@@ -1,9 +0,0 @@
-# from config import settings
-# from grifon.mqbroker.kafka_client import KafkaClient
-#
-# kafka_client = KafkaClient(f"localhost:{settings.KAFKA_CLIENT_PORT}")
-#
-#
-# async def main():
-#     await kafka_client.start_handling()
-#
