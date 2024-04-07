149cc92b552240fc1d393091ddf2c087d3aceca7
Vorkov Nikita <vorkov711@gmail.com>
2024-04-07 16:46:53 +0300

    working version

--- db/models.py
+++ db/models.py
@@ -1,18 +1,18 @@
-
-from sqlalchemy import (Column, DateTime,
-                        Integer, String, Boolean, ForeignKey, Date, JSON, UniqueConstraint, Enum, Numeric,
-                        BigInteger, Float)
-from sqlalchemy import Index
-from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy.orm import relationship, declared_attr, declarative_base
-
-
-Base = declarative_base()
-
-
-class User(Base):
-    __tablename__ = "user"
-
-    id = Column(String(), primary_key=True)
-    name = Column(String())
-    embedding = Column(ARRAY(Float), nullable=False, comment='Эмбеддинг юзера')
+#
+# from sqlalchemy import (Column, DateTime,
+#                         Integer, String, Boolean, ForeignKey, Date, JSON, UniqueConstraint, Enum, Numeric,
+#                         BigInteger, Float)
+# from sqlalchemy import Index
+# from sqlalchemy.dialects.postgresql import ARRAY
+# from sqlalchemy.orm import relationship, declared_attr, declarative_base
+#
+#
+# Base = declarative_base()
+#
+#
+# class User(Base):
+#     __tablename__ = "user"
+#
+#     id = Column(String(), primary_key=True)
+#     name = Column(String())
+#     embedding = Column(ARRAY(Float), nullable=False, comment='Эмбеддинг юзера')
--- tasks/video_analyse.py
+++ tasks/video_analyse.py
@@ -1,29 +1,36 @@
-from config import settings
-from grifon.video_analysis.schema import VideoAnalyseMessage
-from grifon.recommendation.schema import CreateUserRecommendationMessage
-from db.utils import session
-from db.models import User
-import logging
-from tasks import kafka_client  # Импорт экземпляра KafkaClient
 import json
 
+from config import settings
+from grifon.video_analysis.schema import VideoAnalysisMessage
+from grifon.recommendation.schema import CreateUserRecommendationMessage, UserRecommendationMessage
+# from db.utils import session
+# from db.models import User
+import logging
+from tasks import kafka_client  # Импорт экземпляра KafkaClient
 
-async def _get_recommendations(message: CreateUserRecommendationMessage):
-    kafka_client.send_message(settings.RECOMMENDATION_TOPIC, message)
+
+async def _request_recommendations(message: CreateUserRecommendationMessage):
+    kafka_client.send_message(settings.RECOMMENDATION_REQUEST_TOPIC, message)
     kafka_client.flush()
 
 
 @kafka_client.register_topic_handler(settings.VIDEO_ANALYSIS_TOPIC)
-async def get_recommendations(msg: VideoAnalyseMessage):
+async def get_video_data(msg: VideoAnalysisMessage):
     """
     1. Получение внутреннего ID пользователя по эмбедингу
     2. Передача ID пользователя с данными из видео анализа в сервис формирования рекомендаций
     """
     logging.info("Start recommendation processing...")
+    msg = CreateUserRecommendationMessage.parse_obj(json.loads(msg.value()) | dict(user_id=3))
 
-    msg = VideoAnalyseMessage.parse_obj(json.loads(msg.value()))
-    target_embedding = msg.embedding
-    user_id = session.query(User.id).filter(User.embedding == target_embedding).all()
-
-    await _get_recommendations(msg)     # todo: upd message
+    await _request_recommendations(msg)     # todo: upd message
     logging.info("Sent task to recommendation service")
+
+
+@kafka_client.register_topic_handler(settings.RECOMMENDATION_RESPONSE_TOPIC)
+async def get_recommendations(msg: UserRecommendationMessage):
+
+    logging.info("Start recommendation processing...")
+    msg = UserRecommendationMessage.parse_obj(json.loads(msg.value()))
+
+    logging.info(msg)
