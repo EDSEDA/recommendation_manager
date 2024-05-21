638e873e0daa9757e4962147985bbb3535084845
niksul <nikita.sulimenko@smddev.com>
2024-05-21 22:54:49 +0300

    add structured logs

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 import asyncio
+import websockets
 
 import tasks.video_analyse  # noqa
 from tasks import start_handling
+from grifon.log import get_logger
+
+logger = get_logger(__name__)
+
+
+async def echo(websocket, path):
+    async for message in websocket:
+        print("Получено сообщение от клиента:", message)
+        await websocket.send("Эхо: " + message)
+
+
+async def main():
+    start_server = websockets.serve(echo, "localhost", 8765)
+
+    consumer_task = asyncio.create_task(start_handling())
+
+    await asyncio.gather(start_server, consumer_task, )
 
 
 if __name__ == "__main__":
-    asyncio.run(start_handling())
+    logger.info(f'Init recommendation manager')
+    asyncio.run(main())
 
 
 
--- tasks/video_analyse.py
+++ tasks/video_analyse.py
@@ -1,10 +1,12 @@
 from config import settings
-from grifon.video_analysis.schema import VideoAnalyseMessage
+from grifon.video_analysis.schema import VideoAnalysisMessage
 from grifon.recommendation.schema import CreateUserRecommendationMessage, UserRecommendationMessage
 # from db.utils import session
 # from db.models import User
-import logging
 from tasks import kafka_client
+from grifon.log import get_logger
+
+logger = get_logger(__name__)
 
 
 async def _get_recommendations(message: CreateUserRecommendationMessage):
@@ -12,20 +14,20 @@ async def _get_recommendations(message: CreateUserRecommendationMessage):
     kafka_client.flush()
 
 
-@kafka_client.register_topic_handler(settings.VIDEO_ANALYSIS_TOPIC, msg_class=VideoAnalyseMessage)
-async def get_recommendations_from_video(msg: VideoAnalyseMessage):
+@kafka_client.register_topic_handler(settings.VIDEO_ANALYSIS_TOPIC, msg_class=VideoAnalysisMessage)
+async def get_recommendations_from_video(msg: VideoAnalysisMessage):
     """
     1. Получение внутреннего ID пользователя по эмбедингу
     2. Передача ID пользователя с данными из видео анализа в сервис формирования рекомендаций
     """
-    logging.info("Start recommendation processing...")
+    logger.info("Start recommendation processing...")
 
     # target_embedding = msg.embedding
     # user_id = session.query(User.id).filter(User.embedding == target_embedding).all()
 
     get_rec_msg = CreateUserRecommendationMessage.parse_obj(dict(msg) | dict(user_id=1))
     await _get_recommendations(get_rec_msg)
-    logging.info("Sent task to recommendation service")
+    logger.info("Sent task to recommendation service")
 
 
 @kafka_client.register_topic_handler(settings.RECOMMENDATION_RESPONSE_TOPIC, msg_class=UserRecommendationMessage)
@@ -33,6 +35,6 @@ async def get_recommendations_from_rec_service(msg: UserRecommendationMessage):
     """
     1. Рекомендации получены от сервиса рекомендаций
     """
-    logging.info(f"Recommendation for {msg.user_id} is: {msg.recommendations}")
+    logger.info(f"Recommendation for {msg.user_id} is: {msg.recommendations}")
     return
 
